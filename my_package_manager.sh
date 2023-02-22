1fdd49c752590353ffb35069934b51e22c43a9e7
Frederik Petersen <frederik.greve@gmail.com>
2023-02-22 15:10:11 +0100

    Made prompts for the initial information

--- my_package_manager.sh
+++ my_package_manager.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
+USING_SOURCE=0
 
+## Ask for folder name
+echo 'What should the package folder be called?'
+read -p 'Folder: ' FOLDER_NAME
+
+## Ask if installing using source or dpkg
+read -p 'Do you want to use source? Default is using dpkg (y/N) ' yN
+case $yN in
+	[yY] )  echo "Using source"
+		USING_SOURCE=1
+		;;
+	[nN] )  echo "Using dpkg"
+		;;
+	*    )  echo "Using dpkg" 
+		;;
+esac	
+
+## Ask for link to package file
+echo 'Please enter the link to the package download: '
+read -p 'Link: ' LINK
+
+BASENAME=$(basename $LINK)
+
+echo $BASENAME
